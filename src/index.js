22353071ebe3c09dd4cbcab67357432db3bb98af
Irene <irinaselen1@gmail.com>
2020-10-02 13:42:08 +0300

    fix: solve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min (array) {    
+    if (array === undefined || array.length === 0) {
+    return 0;
+    }     
+     let min = Math.min.apply(null, array);
+    return min;        
+};
 
 exports.max = function max (array) {
+    if (array === undefined || array.length === 0) {
   return 0;
-}
+    } 
+    let max = Math.max.apply(null, array);
+    return max; 
+   
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    if (array === undefined || array.length === 0) {
+    return 0;
+  } 
+    let sum = 0; 
+    for (let i = 0; i < array.length; i++) {
+    sum += parseInt( array[i], 10 ); }
+
+    let avg = sum/array.length;
+    return avg; 
+  };
+
+
+
+
+
+
+
